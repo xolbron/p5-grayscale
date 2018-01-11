2aa5640bc71bbbf5ff59ac313f6a0fbe3a97d43f
Jerome Foster II <jfoster@wlapcs.org>
2018-01-11 03:18:07 +0000

    built greyscale demo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
     createCanvas(600,400);
+    background(255);
+    noStroke();
+    num = 0;
+    frameRate(80);
 }
 
-function draw() {
-  ellipse(50, 50, 80, 80);
+function draw(){
+    if(num < 256){
+        fill(num);
+        ellipse(width/2, height/2, 250, 250);
+       
+        fill('#FF0000');
+        textSize(25);
+        textAlign(CENTER, CENTER);
+        text(num, width/2, height/2);
+        
+        num = num + 1;
+    }
 }
